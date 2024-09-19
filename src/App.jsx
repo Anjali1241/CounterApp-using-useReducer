8b8fe1c05927954a2bd56771f697e269db05fd7c
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-09-19 23:40:53 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { useReducer, } from 'react'
 
 import './App.css'
-
-function App() {
-
-  const initialState = { count: 0 }
-  const reducer = (state, action) => {
-    if (action.type == "INCREMENT") {
-      return { count: action.payload + state.count }
-    }
-    if (action.type == "DECREMENT") {
+const initialState = { count: 0 }
+const reducer = (state, action) => {
+  if (action.type == "INCREMENT") {
+    return { count: action.payload + state.count }
+  }
+  if (action.type == "DECREMENT") {
+    if (state.count > 0) {
       return { count: state.count - action.payload }
     }
-    if (action.type == "RESET") {
-      return { count: action.payload }
-    }
+    return {count :0}
   }
+  if (action.type == "RESET") {
+    return { count: action.payload }
+  }
+}
+function App() {
+
+
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className='bg-slate-400 h-screen
      text-center flex flex-col items-center justify-center'>
-      <h1 className="my-2 text-4xl">App using useReducer hook</h1>
+      <h1 className="my-2 text-5xl">App using useReducer hook</h1>
       <p className='text-4xl my-4 animate-bounce'>{state.count}</p>
       <div className="text-white">
         <button className="bg-slate-800 m-2 p-2 rounded-md hover:bg-slate-100 hover:text-black" onClick={() => dispatch({ type: "INCREMENT", payload: 1 })}>Increment</button>
